5c82d06ddb41c14e36335da9f09be64390fd8a00
User850413 <csyou1228@gmail.com>
2024-08-30 17:24:57 +0900

    08/30 :sparkles: : layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import Projects from "./pages/projects";
 import Connections from "./pages/connections";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/connections" element={<Connections />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/connections" element={<Connections />} />
+        </Route>
       </Routes>
     </>
   );
