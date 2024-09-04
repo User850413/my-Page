7e3931a6b18ac2685895d3f523c2bbb865345313
User850413 <csyou1228@gmail.com>
2024-09-04 12:47:32 +0900

    09/04 :lipstick: : 메인 톱니바퀴 애니메이션

---
+++ src/pages/main/MainAnimation.tsx
@@ -0,0 +1,22 @@
+import { motion } from 'framer-motion';
+
+import ico_wheel from '../../assets/icons/ico_wheel.svg';
+
+function MainAnimation() {
+  return (
+    <div className='relative left-0 right-0 top-0 h-screen bg-bright'>
+      <motion.span
+        transition={{ duration: 1.5, ease: 'easeInOut' }}
+        initial={{ x: '-50%', y: '-50%', rotate: 0 }}
+        animate={{ rotate: [0, 50, -180, 0] }}
+        className='absolute left-1/2 top-1/2 w-[350px]'
+      >
+        <img src={ico_wheel} alt='톱니' />
+      </motion.span>
+      <motion.span className='difference absolute left-[50px] top-[216px] cursor-default text-[30px] font-black text-bright tablet:text-[60px] desktop:text-[120px]'>CHOI SOO HYEONG</motion.span>
+      <motion.span className='difference absolute left-[50px] top-[330px] cursor-default text-[20px] font-black text-bright tablet:text-[60px] desktop:text-[80px]'>PORTFOLIO</motion.span>
+    </div>
+  );
+}
+
+export default MainAnimation;
