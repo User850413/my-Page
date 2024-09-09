2633f75b41b646799c6981bb8e3cb68d103614d8
User850413 <csyou1228@gmail.com>
2024-09-09 21:06:33 +0900

    09/09 :lipstick: : 프로젝트 카드 UI

--- src/pages/projects/ProjectDetail.tsx
+++ src/pages/projects/ProjectDetail.tsx
@@ -1,3 +1,5 @@
+import { PropsWithChildren } from 'react';
+
 import ico_React from '../../assets/icons/ico_React.svg';
 import ico_bolt from '../../assets/icons/ico_bolt.svg';
 import ico_js from '../../assets/icons/ico_js.svg';
@@ -14,6 +16,11 @@ interface TitleProps {
   tools: string[];
 }
 
+interface DescLabelProps extends PropsWithChildren {
+  text: string;
+  flex: string;
+}
+
 const ToolIcon = ({ tool }: { tool: string }) => {
   const toolToIcon: { [tool: string]: string } = {
     typescript: ico_ts,
@@ -21,7 +28,7 @@ const ToolIcon = ({ tool }: { tool: string }) => {
     tailwind: ico_tailwind,
     javascript: ico_js,
     mantine: ico_mantine,
-    nextjs: ico_next,
+    next: ico_next,
   };
 
   return <img src={toolToIcon[tool]} alt={tool} className='h-full w-full' />;
@@ -29,36 +36,43 @@ const ToolIcon = ({ tool }: { tool: string }) => {
 
 const ProjectTitleCard = ({ title, startDate, lastDate, tools }: TitleProps) => {
   return (
-    <>
-      <div className='group relative mx-auto flex w-[266px] flex-col gap-[10px] py-[10px] tablet:w-[698px] tablet:flex-row tablet:gap-[30px] tablet:py-[56px] tablet:pl-[67px] desktop:w-[1200px] desktop:gap-[40px] desktop:py-[74px] desktop:pl-[98px]'>
-        <img
-          src={ico_bolt}
-          alt='볼트 이미지'
-          className='absolute left-0 top-[188px] w-[16px] -translate-y-1/2 transition-all group-hover:w-[20px] group-hover:rotate-90 tablet:top-1/2 tablet:w-[25px] group-hover:tablet:w-[30px] desktop:w-[40px] group-hover:desktop:w-[60px]'
-        />
-        <img
-          src={ico_bolt}
-          alt='볼트 이미지'
-          className='absolute right-0 top-[188px] w-[16px] -translate-y-1/2 transition-all group-hover:w-[20px] group-hover:rotate-90 tablet:top-1/2 tablet:w-[25px] group-hover:tablet:w-[30px] desktop:w-[40px] group-hover:desktop:w-[60px]'
-        />
-        <span className='h-[160px] w-[266px] bg-red-300 desktop:h-[202px] desktop:w-[336px]'>thumbnail</span>
-        <div className='flex flex-col items-center justify-center gap-[10px] tablet:items-start desktop:gap-[22px]'>
-          <div className='flex flex-col tablet:items-start desktop:flex-row desktop:items-end desktop:gap-[5px]'>
-            <h3 className='text-[24px] tablet:mb-[-10px] tablet:text-[30px] desktop:mb-[-5px] desktop:text-[40px]'>{title}</h3>
-            <span className='hidden text-[21px] font-light tablet:block desktop:text-[24px]'>
-              {startDate} - {lastDate}
-            </span>
-          </div>
-          <ul className='hidden items-center gap-[15px] tablet:flex'>
-            {tools.map((tool, index) => (
-              <li key={index} className='h-[60px] w-[60px] desktop:h-[80px] desktop:w-[80px]'>
-                <ToolIcon tool={tool} />
-              </li>
-            ))}
-          </ul>
+    <div className='group relative mx-auto flex w-[266px] flex-col gap-[10px] py-[10px] tablet:w-[698px] tablet:flex-row tablet:gap-[30px] tablet:border-b-[1px] tablet:border-b-middle tablet:py-[56px] tablet:pl-[67px] desktop:w-[1200px] desktop:gap-[40px] desktop:py-[74px] desktop:pl-[98px]'>
+      <img
+        src={ico_bolt}
+        alt='볼트 이미지'
+        className='absolute left-0 top-[188px] w-[16px] -translate-y-1/2 transition-all group-hover:w-[20px] group-hover:rotate-90 tablet:top-1/2 tablet:w-[25px] group-hover:tablet:w-[30px] desktop:w-[40px] group-hover:desktop:w-[60px]'
+      />
+      <img
+        src={ico_bolt}
+        alt='볼트 이미지'
+        className='absolute right-0 top-[188px] w-[16px] -translate-y-1/2 transition-all group-hover:w-[20px] group-hover:rotate-90 tablet:top-1/2 tablet:w-[25px] group-hover:tablet:w-[30px] desktop:w-[40px] group-hover:desktop:w-[60px]'
+      />
+      <span className='h-[160px] w-[266px] bg-red-300 desktop:h-[202px] desktop:w-[336px]'>thumbnail</span>
+      <div className='flex flex-col items-center justify-center gap-[10px] tablet:items-start desktop:gap-[22px]'>
+        <div className='flex flex-col tablet:items-start desktop:flex-row desktop:items-end desktop:gap-[5px]'>
+          <h3 className='text-[24px] tablet:mb-[-10px] tablet:text-[30px] desktop:mb-[-5px] desktop:text-[40px]'>{title}</h3>
+          <span className='hidden text-[21px] font-light tablet:block desktop:text-[24px]'>
+            {startDate} - {lastDate}
+          </span>
         </div>
+        <ul className='hidden items-center gap-[15px] tablet:flex'>
+          {tools.map((tool, index) => (
+            <li key={index} className='h-[60px] w-[60px] desktop:h-[80px] desktop:w-[80px]'>
+              <ToolIcon tool={tool} />
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
+  );
+};
+
+const DescLabel = ({ text, flex: direct, children }: DescLabelProps) => {
+  return (
+    <div className={`flex gap-[10px] ${direct}`}>
+      <span className='w-fit rounded-[4px] bg-middle p-[6px_12px] text-[18px] tablet:text-[24px] desktop:text-[32px]'>{text}</span>
+      <div className={`text-[18px] font-light tablet:text-[24px] desktop:text-[32px] ${direct == 'row' ? 'mt-[6px]' : ''}`}>{children}</div>
+    </div>
   );
 };
 
@@ -68,16 +82,46 @@ const ProjectContentCard = ({ title, startDate, lastDate, tools, point, descript
   };
 
   return (
-    <>
-      <a onClick={navigateToLink}>github로</a>
-      <div>{title}</div>
-      <div>
-        {startDate} - {lastDate}
+    <div className='relative py-[10px] after:absolute after:left-1/2 after:block after:w-[166px] after:-translate-x-1/2 after:border-b-[1px] after:border-middle tablet:mt-[40px] tablet:py-0 tablet:after:w-[264px] desktop:mt-[200px] desktop:after:w-full'>
+      <div onClick={navigateToLink} className='mx-auto h-[160px] w-[266px] cursor-pointer border-[1px] border-black tablet:h-[342px] tablet:w-[608px] desktop:h-[524px] desktop:w-[930px]'>
+        thumbnail
       </div>
-      <div>{tools}</div>
-      <div>{point}</div>
-      <div>{description}</div>
-    </>
+      <h2 className='mx-auto mb-[25px] mt-[10px] w-fit text-[20px] tablet:mt-[20px] tablet:text-[40px] desktop:mb-[40px] desktop:mt-[100px] desktop:text-[60px]'>{title}</h2>
+      <div className='flex flex-col items-start gap-[20px]'>
+        <div className='flex w-full flex-col items-start gap-[20px] desktop:flex-row'>
+          <div className='flex flex-1 flex-col items-start gap-[20px]'>
+            <DescLabel text='기간' flex='flex-row'>
+              <div>
+                {startDate} - {lastDate}
+              </div>
+            </DescLabel>
+            <DescLabel text='특징' flex='flex-row'>
+              <div>{point}</div>
+            </DescLabel>
+          </div>
+          <div className='flex-1 desktop:border-l-[1px] desktop:border-l-middle desktop:pl-[30px]'>
+            <DescLabel text='사용 기술' flex='flex-col'>
+              <ul className='flex gap-[9px]'>
+                {tools.map((tool, index) => (
+                  <li key={index} className='h-[40px] w-[40px] tablet:h-[80px] tablet:w-[80px]'>
+                    <ToolIcon tool={tool} />
+                  </li>
+                ))}
+              </ul>
+            </DescLabel>
+          </div>
+        </div>
+        <DescLabel text='설명' flex='flex-col'>
+          <div>{description}</div>
+        </DescLabel>
+      </div>
+      <a
+        className='mx-auto mb-[10px] mt-[60px] block w-fit cursor-pointer rounded-[50px] bg-middle px-[21px] py-[8px] text-[18px] hover:bg-middleDarker tablet:mb-[30px] tablet:mt-[100px] tablet:px-[40px] tablet:text-[24px] desktop:mb-[40px] desktop:mt-[200px] desktop:px-[50px] desktop:text-[32px]'
+        onClick={navigateToLink}
+      >
+        GitHub
+      </a>
+    </div>
   );
 };
 
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,24 @@
+import Accordion from '../../components/Accordion';
+import PROJECTS from '../../constants/projects';
+import { ProjectContentCard, ProjectTitleCard } from './ProjectDetail';
+
 function Projects() {
-  return <div>프로젝트 페이지입니다.</div>;
+  return (
+    <div className='mx-auto mt-[120px] w-fit'>
+      <Accordion>
+        {PROJECTS.map(({ title, lastDate, startDate, description, tools, point, id, URL }) => (
+          <div className='w-min' key={id}>
+            <Accordion.Title id={id}>
+              <ProjectTitleCard title={title} lastDate={lastDate} startDate={startDate} tools={tools} />
+            </Accordion.Title>
+            <Accordion.Content id={id}>
+              <ProjectContentCard id={id} title={title} lastDate={lastDate} startDate={startDate} tools={tools} description={description} point={point} URL={URL} />
+            </Accordion.Content>
+          </div>
+        ))}
+      </Accordion>
+    </div>
+  );
 }
 
 export default Projects;
